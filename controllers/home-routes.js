1be862656c3ed5762557e4d2792afd8d1a0b919c
mhans19 <mdhansen19@gmail.com>
2020-08-02 15:39:24 -0600

    added routes within controllers - simulated from just-tech-news

---
+++ controllers/home-routes.js
@@ -0,0 +1,106 @@
+//DEPENDENCIES
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+// GET ALL
+router.get('/', (req, res) => {
+    console.log(req.session);
+    Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'body',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 
+                       'comment_text', 
+                       'post_id', 
+                       'user_id', 
+                       'created_at'
+                      ],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        // pass a single post object into the homepage template
+        res.render('homepage', {
+          posts,
+          loggedIn: req.session.loggedIn
+        });
+        router.get('/login', (req, res) => {
+          res.render('login');
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+// GET LOGIN
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+  });
+// GET ONE POST  
+  router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'title',
+        'body',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+  
+        // pass data to template
+        res.render('single-post', {
+          post,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+// MODULE EXPORTS  
+  module.exports = router;
