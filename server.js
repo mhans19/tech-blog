d677266a7b02bc6829fd0972f7ba8e318c7b155a
mhans19 <mdhansen19@gmail.com>
2020-08-04 00:12:32 -0600

    session expires after 30 minutes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set('view engine', 'handlebars')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret',
-  cookie: {},
+  cookie: {expires: 30 * 60 * 1000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
